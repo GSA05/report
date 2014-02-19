eb4b985c8035c8540723bdfe0d3a1a0c953137a0
GSA05 <GSA05@mail.ru>
2014-02-19 18:27:15 +0400

    Всякие улучшения
    Signed-off-by: GSA05 <GSA05@mail.ru>

--- report.pl
+++ report.pl
@@ -5,6 +5,7 @@ use Excel::Writer::XLSX;
 
 my $thread_num = 8;
 my $student = 1.3862;
+my $NG = 5;
 
 my $i = 0;
 my $j = 0;
@@ -12,26 +13,59 @@ my $k = 0;
 my %time = undef;
 my $file;
 
+my %functions = (
+    CONST => {
+    },
+    INIT => {
+        SIZE => 'NH',
+        },
+    SOURCE => {
+        SIZE => 'NH',
+        },
+    GMATR => {
+        SIZE => 'NG*NG',
+        },
+    GMATRF => {
+        SIZE => 'NG*NG',
+        },
+    GMATRL => {
+        SIZE => 'NG*NG',
+        },
+    GMATR_F => {
+        SIZE => 'NG*NG',
+        },
+    GMATR_C => {
+        SIZE => 'NG*NG',
+        },
+    INTEG => {
+        SIZE => 'NH',
+        },
+    INTEGL => {
+        SIZE => 'NH',
+        },
+    );
+
+sub sum {
+    my ($data,$pow) = @_;
+    $pow = $pow || 1;
+    my $s = 0;        
+    foreach ( @$data ) { $s += $_**$pow }
+    return $s;
+}
+
 sub mean {
     my $data = shift;
-    my $n = 3;
-    my $m = 0;
-        
-    for ( my $i = 0; $i < $n; $i++ ) { $m += $data->[$i] }
+    my $n = @$data;
+    my $m = sum($data);
     $m /= $n;
     return $m;
 }
 
 sub variance {
     my $data = shift;
-    my $n = 3;
-    my $s = 0;
-    my $v = 0;
-    
-    for ( my $i = 0; $i < $n; $i++ ) {
-        $v += $data->[$i]**2;
-        $s += $data->[$i];
-    }
+    my $n = @$data;
+    my $s = sum($data);
+    my $v = sum($data,2);
     $v = ($v - $s**2/$n)/($n-1);
     return $v;
 }
@@ -69,36 +103,6 @@ for $i ( values %time ) {
     }
 }
 
-my %functions = (
-    INIT => {
-        SIZE => 'NH',
-        },
-    SOURCE => {
-        SIZE => 'NH',
-        },
-    GMATR => {
-        SIZE => 'NG*NG',
-        },
-    GMATRF => {
-        SIZE => 'NG*NG',
-        },
-    GMATRL => {
-        SIZE => 'NG*NG',
-        },
-    GMATR_F => {
-        SIZE => 'NG*NG',
-        },
-    GMATR_C => {
-        SIZE => 'NG*NG',
-        },
-    INTEG => {
-        SIZE => 'NH',
-        },
-    INTEGL => {
-        SIZE => 'NH',
-        },
-    );
-    
 #print Dumper(%functions);
 
 my $workbook = Excel::Writer::XLSX->new('report.xlsx');
@@ -119,7 +123,9 @@ my $mean0;
 my $var0;
 my $mean;
 my $var;
-foreach $key ( keys %time ) {
+
+foreach $key ( sort keys %time ) {
+    my $f = 0;
     my $worksheet = $workbook->add_worksheet($key);
     for ( $row = 0; $row < @{$time{$key}}; $row++) {
         $col = 0;
@@ -133,6 +139,11 @@ foreach $key ( keys %time ) {
                 $worksheet->write($row,$k * 1 + $col++,$name,$format);
                 if ( defined $functions{$name} ) {
                     $functions{$name}{'POSITION'} = $col - 1;
+                    $functions{$name}{'SIZE'} =~ s/NG/$NG/g;
+                    $functions{$name}{'SIZE'} =~ s/NH/$key/g;
+                    $functions{$name}{'SIZE'} = eval $functions{$name}{'SIZE'};
+                    $functions{'CONST'}{$key}{'PROC_MEAN'}[$f] = $time{$key}->[0]{$name}{'PROC_MEAN'};
+                    $functions{'CONST'}{$key}{'PROC_VAR'}[$f++] = $time{$key}->[0]{$name}{'PROC_VAR'};
                 }
                 $worksheet->write($row,$k * 1 + $col++,'DATA1',$format);
                 $worksheet->write($row,$k * 1 + $col++,'DATA2',$format);
@@ -160,7 +171,7 @@ foreach $key ( keys %time ) {
             $k++;
         }
     }
-    
+    $functions{'CONST'}{$key}{'PROC'} = 100 - sum($functions{'CONST'}{$key}{'PROC_MEAN'});
 }
 
-#print Dumper(%functions);
+print Dumper(%functions);
